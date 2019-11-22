a18493fdad418d111359f16e7363aa545a060283
Varun Resh Maddali <mvresh@gmail.com>
2019-11-22 12:01:14 +0530

    done with test cases

--- bin/main.dart
+++ bin/main.dart
@@ -21,5 +21,85 @@
 /// Output: [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],
 /// ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
 
+import "dart:collection";
+class Account {
+  String accountName;
+  List<String> accountEmailIds;
+  Account({this.accountName, this.accountEmailIds});
+
+  bool haveCommonEmailIds(Account account1) {
+    return account1.accountEmailIds.any((emailId) => this.accountEmailIds.contains(emailId));
+  }
+
+
+}
+
+
+Account mergeAccounts(Account account1, Account account2) {
+  Account mergedAccount = Account();
+
+  if(account1.accountName.contains(account2.accountName)){
+    mergedAccount.accountName = account1.accountName;
+  }
+
+  else if(account2.accountName.contains(account1.accountName)){
+    mergedAccount.accountName = account2.accountName;
+  }
+
+  else{
+    mergedAccount.accountName = account1.accountName + account2.accountName;
+  }
+
+  mergedAccount.accountEmailIds = account1.accountEmailIds.toList()..addAll(account2.accountEmailIds.toList());
+  mergedAccount.accountEmailIds = LinkedHashSet<String>.from(mergedAccount.accountEmailIds).toList();
+
+  return mergedAccount;
+}
+
+List<String> accountToList(Account account){
+  List<String> sameAccountList = [account.accountName];
+  sameAccountList.addAll(account.accountEmailIds);
+  return sameAccountList;
+}
+
+List<Account> normalListToAccountList(List<List<String>> normalList){
+  List<Account> accountsList = [];
+  for(int i = 0;i<normalList.length;i++){
+    Account newAccount = Account();
+    newAccount.accountName = normalList[i][0];
+    newAccount.accountEmailIds = normalList[i].sublist(1);
+    accountsList.add(newAccount);
+  }
+  return accountsList;
+}
+
+List<List<String>> removeDuplicateAccounts(
+    List<List<String>> originalAccountsList) {
+
+  List<Account> accountsList = normalListToAccountList(originalAccountsList);
+  List<List<String>> finalAccountsList = [];
+
+  for(int i = 0 ; i < accountsList.length ; i++){
+    for(int j = i+1; j < accountsList.length ; j++){
+      if(accountsList[i].haveCommonEmailIds(accountsList[j])){
+        accountsList[i] = mergeAccounts(accountsList[i], accountsList[j]);
+        accountsList.removeAt(j);
+        j--;
+
+      }
+    }
+    finalAccountsList.add(accountToList(accountsList[i]));
+  }
+
+  return finalAccountsList;
+}
+
 main() {
+  print(removeDuplicateAccounts([["John", "johnsmith@mail.com", "john00@mail.com"],
+    ["John", "johnnybravo@mail.com"], ["John", "johnsmith@mail.com", "john_newyork@mail.com"],["John", "johnsmith@mail.com", "john00@mail.com"],
+    ["Mary", "mary@mail.com"]]));
+
+  print(removeDuplicateAccounts([["John", "j@b.com", "john00@mail.com"],
+    ["John", "johnnybravo@mail.com"], ["John V", "johnsmith@mail.com", "j@b.com"],
+    ["Mary", "mary@mail.com"]]));
 }
--- test/Day_36_Morning_Challenge_test.dart
+++ test/Day_36_Morning_Challenge_test.dart
@@ -1,6 +1,67 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
-  test('Test Case', () {
+  test('Test Case 1', () {
+    expect(
+        removeDuplicateAccounts([
+          ["John", "johnsmith@mail.com", "john00@mail.com"],
+          ["John", "johnnybravo@mail.com"],
+          ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+          ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+          ["Mary", "mary@mail.com"]
+        ]),
+        [
+          [
+            'John',
+            'johnsmith@mail.com',
+            'john00@mail.com',
+            'john_newyork@mail.com'
+          ],
+          ['John', 'johnnybravo@mail.com'],
+          ['Mary', 'mary@mail.com']
+        ]);
+  });
+
+  test('Test Case 2', () {
+    expect(
+        removeDuplicateAccounts([
+          ["John", "j@b.com", "john00@mail.com"],
+          ["John", "johnnybravo@mail.com"],
+          ["John V", "johnsmith@mail.com", "j@b.com"],
+          ["Mary", "mary@mail.com"]
+        ]),
+        [
+          [
+            'John V',
+            'j@b.com',
+            'john00@mail.com',
+            "johnsmith@mail.com"
+          ],
+          ['John', 'johnnybravo@mail.com'],
+          ['Mary', 'mary@mail.com']
+        ]);
+  });
+
+  test('Test Case 3', () {
+    expect(
+        removeDuplicateAccounts([
+          ["John", "j@b.com", "john00@mail.com"],
+          ["John", "j@b.com", "jon00@mail.com"],
+          ["John", "johnnybravo@mail.com"],
+          ["John V", "johnsmith@mail.com", "j@b.com"],
+          ["Mary", "mary@mail.com"]
+        ]),
+        [
+          [
+            'John V',
+            'j@b.com',
+            'john00@mail.com',
+            'jon00@mail.com',
+            "johnsmith@mail.com"
+          ],
+          ['John', 'johnnybravo@mail.com'],
+          ['Mary', 'mary@mail.com']
+        ]);
   });
 }
